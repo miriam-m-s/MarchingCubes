257f26a38348b529d8bb8a62b88ecb9b4823914f
MiriamMartín <mirima10@ucm.es>
2025-05-29 00:30:33 +0200

    firts commit

---
+++ MarchingCubes/Source/MarchingCubes/MarchingCubes.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MarchingCubes.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MarchingCubes, "MarchingCubes" );
+
+DEFINE_LOG_CATEGORY(LogMarchingCubes)
+ 
